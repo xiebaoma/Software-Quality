9df8a5aebb4cc88da4fe71487328e6409b4a136c
xiebaoma <2402583447@qq.com>
2025-12-31 14:02:36 +0800

    fucking end

---
+++ git_init.sh
@@ -0,0 +1,7 @@
+echo "# Software-Quality" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/xiebaoma/Software-Quality.git
+git push -u origin main
